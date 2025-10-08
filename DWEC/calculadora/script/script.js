8ae4ae59455ad648d4138d594f7db7e1d631c3dd
Jose Miguel Arras <jose.arras.gavira.al@iespoligonosur.org>
2025-10-08 10:10:29 +0200

    calculadora DWEC

---
+++ DWEC/calculadora/script/script.js
@@ -0,0 +1,15 @@
+const pantalla = document.getElementById('pantalla');
+
+function agregar(valor) {
+  pantalla.value += valor;
+}
+
+function borrarTodo() {
+  pantalla.value = '';
+}
+
+function mostrarResultado() {
+  pantalla.value = eval(pantalla.value);
+ 
+}
+
